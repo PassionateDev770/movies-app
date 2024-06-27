9691c974798f5d4713fc5137bed7536c3dc91efa
PassionateDev770 <alimatousadiyadiaho@gmail.com>
2024-06-27 22:02:11 +0200

    add data in database

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -41,7 +41,7 @@ class MovieController extends Controller
 
     public function getMoviesFromApi()
     {
-        //n'ayant pas vu l'url fourni dans l'énoncé, j'ai pris celui-ci du même api
+        //n'ayant pas vu l'url fourni dans l'énoncé dans l'API, j'ai pris celui-ci qui correspondait le mieux du même api
         $url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
         $token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjJkNjNjZGRjMDY2ZDk5ZWQzZTgwNmQzMjY3MThjYSIsInN1YiI6IjYyNGVhNTRhYjc2Y2JiMDA2ODIzODc4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zuuBq1c63XpADl8SQ_c62hezeus7VibE1w5Da5UdYyo';
 
